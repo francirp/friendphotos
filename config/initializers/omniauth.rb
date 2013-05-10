981083a80de79dae78dc5fa546ddc17cfe9bb032
Ryan Francis <ryanfrancis@Ryans-MacBook-Pro.local>
2013-05-09 20:26:55 -0500

    enhanced photo quality

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '449993895087372', '5be63f37b39be7905015608598fc41e2',
            :scope => 'email,user_photos,friends_photos', :display => 'popup'
 end
+
+#production app id: 449993895087372
+#production app secret: 5be63f37b39be7905015608598fc41e2
+
+
+#test: app id 604342566245467
+#test: app secret: e2ec688e66b71c421733498a00513116
